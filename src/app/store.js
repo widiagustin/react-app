450140330e45074a8810ddc2f644e9590e9b58a8
widiagustin <widhianiagustin14@gmail.com>
2022-10-25 12:01:43 +0700

    feat: import thunk & apply middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
-import { combineReducers, compose, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
 import counterReducer from "../app/features/Counter/reducer"
 
 let rootReducer = combineReducers({
   counter: counterReducer
 })
 
-const composeEnhancers = window.__REDUX_DEVTOOL_EXTENSION_COMPOSE__ || compose
-let store = createStore(rootReducer, composeEnhancers())
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store
