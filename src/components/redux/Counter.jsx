7daa01d2c21df1e5961c8f3f75311404752382a8
widiagustin <widhianiagustin14@gmail.com>
2022-10-25 12:02:43 +0700

    style: add styling to counter component

--- src/components/redux/Counter.jsx
+++ src/components/redux/Counter.jsx
@@ -8,11 +8,18 @@ const Counter = () => {
 
   return (
     <div>
-      <button onClick={() => dispatch(decrementWithCheckingAction(1))}
-        className='py-2 px-4 bg-blue-500 border text-white font-bold'>-</button>
-      {' '} <span>{count}</span> {' '}
-      <button onClick={() => dispatch(increment(1))}
-        className='py-2 px-4 bg-blue-500 border text-white font-bold'>+</button>
+      <div className='max-w-2xl mx-auto flex flex-col gap-8 justify-center h-screen '>
+        <div>
+          <h1 className='font-bold text-3xl text-blue-500 text-center'>Simple Counter App</h1>
+        </div>
+        <div className='text-center py-8 px-20 border-2 border-gray-400 flex items-center gap-6 mx-auto'>
+          <button onClick={() => dispatch(decrementWithCheckingAction(1))}
+            className='py-2 px-4 bg-blue-500 border text-white font-bold'>-</button>
+          <p className='text-3xl'>{count}</p>
+          <button onClick={() => dispatch(increment(1))}
+            className='py-2 px-4 bg-blue-500 border text-white font-bold'>+</button>
+        </div>
+      </div>
     </div>
   )
 }
