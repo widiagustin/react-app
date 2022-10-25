9f9ec61065824570b5e4ffd5d0cd1e623ec13460
widiagustin <widhianiagustin14@gmail.com>
2022-10-25 11:42:25 +0700

    feat: change counter to using react-redux and add actions features

--- src/components/redux/Counter.jsx
+++ src/components/redux/Counter.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrementWithCheckingAction, increment } from '../../app/features/Counter/actions'
 
 const Counter = () => {
+  let { count } = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <button className='py-2 px-4 bg-blue-500 border text-white font-bold'>-</button>
-      {' '} <span>0</span> {' '}
-      <button className='py-2 px-4 bg-blue-500 border text-white font-bold'>+</button>
+      <button onClick={() => dispatch(decrementWithCheckingAction(1))}
+        className='py-2 px-4 bg-blue-500 border text-white font-bold'>-</button>
+      {' '} <span>{count}</span> {' '}
+      <button onClick={() => dispatch(increment(1))}
+        className='py-2 px-4 bg-blue-500 border text-white font-bold'>+</button>
     </div>
   )
 }
